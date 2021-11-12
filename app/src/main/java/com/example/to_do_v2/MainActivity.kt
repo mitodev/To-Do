2f1907d5383953154cb1b78f9523fedeca3bac52
mitodev <mitodev2003@gmail.com>
2021-11-12 11:32:52 +0100

    init

--- app/src/main/java/com/example/to_do_v2/MainActivity.kt
+++ app/src/main/java/com/example/to_do_v2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.to_do_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.example.to_do_v2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //.........................values
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    //.........................functions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
